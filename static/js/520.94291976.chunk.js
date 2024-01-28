1bd668e594ba9d1b789eae46b9ff8d40915d0572
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2024-01-28 16:46:25 +0000

    Deploying to gh-pages from @ Havrylyuk-UA/goit-react-hw-05-movies@5b900630dc2c1a1065374dd4c12546ce79133ce2 🚀

---
+++ static/js/520.94291976.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("p",{style:{textAlign:"center",color:"#fff"},children:"404 Not Found =("})})}}}]);
+//# sourceMappingURL=520.94291976.chunk.js.map
