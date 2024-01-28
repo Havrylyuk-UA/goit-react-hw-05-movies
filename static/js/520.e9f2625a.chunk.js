2db4819b7adcebd01952c13d38ac8dc96a204bc2
Havrylyuk-UA <142251279+Havrylyuk-UA@users.noreply.github.com>
2024-01-28 12:51:38 +0000

    Deploying to gh-pages from @ Havrylyuk-UA/goit-react-hw-05-movies@72adf3581ec2e5472eb002c9529d88697f44cd3f 🚀

---
+++ static/js/520.e9f2625a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{520:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("p",{style:{textAlign:"center"},children:"404 Not Found"})})}}}]);
+//# sourceMappingURL=520.e9f2625a.chunk.js.map
